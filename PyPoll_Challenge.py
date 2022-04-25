82101fd8b2eb4dfc98ce0e414028a932706dbcb2
squishykins <acolasnto@gmail.com>
2022-04-24 22:06:32 -0400

    Update varibles to be more verbose.

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -93,19 +93,19 @@ with open(file_to_save, "w") as txt_file:
     for countyName in counties:
 
         # 6b: Retrieve the county vote count.
-        cvotes = countyVotes[countyName]
+        county_votes = countyVotes[countyName]
 
         # 6c: Calculate the percentage of votes for the county.
-        vote_percentage = float(cvotes) / float(total_votes) * 100
-        county_results = (f"{countyName}: {vote_percentage:.1f}% ({cvotes:,})\n")
+        vote_percentage = float(county_votes) / float(total_votes) * 100
+        county_results = (f"{countyName}: {vote_percentage:.1f}% ({county_votes:,})\n")
 
          # 6d: Print the county results to the terminal.
         print(county_results)
          # 6e: Save the county votes to a text file.
         txt_file.write(county_results)
          # 6f: Write an if statement to determine the winning county and get its vote count.
-        if (cvotes > winning_c_count): 
-            winning_c_count = cvotes
+        if (county_votes > winning_c_count): 
+            winning_c_count = county_votes
             winning_county = countyName
            
     # 7: Print the county with the largest turnout to the terminal.
